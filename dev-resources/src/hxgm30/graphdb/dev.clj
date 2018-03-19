fc9f52b8cb94320da7b251322dc6b0a4bd4f79e0
Duncan McGreggor <oubiwann@gmail.com>
2018-03-18 23:58:13 -0500

    Start experimenting with connecting to OrientDB.

--- dev-resources/src/hxgm30/graphdb/dev.clj
+++ dev-resources/src/hxgm30/graphdb/dev.clj
@@ -1,6 +1,5 @@
 (ns hxgm30.graphdb.dev
   (:require
-    [clj-odbp.core :as odbp]
     [clojure.data.xml :as xml]
     [clojure.java.io :as io]
     [clojure.pprint :refer [pprint]]
@@ -8,8 +7,56 @@
     [clojurewerkz.ogre.core :as ogre]
     [clojusc.twig :as logger]
     [hxgm30.graphdb.config :as config]
-    [hxgm30.graphdb.server :as server]))
+    [trifl.java :refer [show-methods]])
+  (:import
+    (com.tinkerpop.blueprints.impls.orient OrientGraphFactory)))
 
 (logger/set-level! ['hxgm30] :info)
 
 (def refresh #'repl/refresh)
+
+(def test-spec
+  {:protocol "plocal"
+   :path "./data/databases/test"
+   :user "root"
+   :password "root"
+   :pool {:min-db-instances 1
+          :max-db-instances 10}})
+
+(defn create-factory
+  [spec]
+  (let [factory (new OrientGraphFactory (format "%s:%s"
+                                                (:protocol spec)
+                                                (:path spec)
+                                                (:user spec)
+                                                (:password spec)))]
+    (.setupPool factory
+                (get-in spec [:pool :min-db-instances])
+                (get-in spec [:pool :max-db-instances]))))
+
+(defn destroy-factory
+  [factory]
+  (.close factory))
+
+(defn connect
+  ([factory]
+    (connect factory {}))
+  ([factory opts]
+    (if (not (:transactional? opts))
+      (.getNoTx factory)
+      (.getTx factory))))
+
+(defn disconnect
+  [conn]
+  (.shutdown conn))
+
+(defn add-vertex
+  [graph props]
+  (.addVertex graph props))
+
+(comment
+  (def f (create-factory test-spec))
+  (def g (connect f))
+  (add-vertex g {:type :room :name "A cave" :description "You are in a dark cave."})
+  (disconnect g)
+  (destroy-factory f))
