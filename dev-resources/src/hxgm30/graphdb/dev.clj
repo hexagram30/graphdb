1cd5272d8cb55dc9d709e20f2203efdede5e484a
Duncan McGreggor <oubiwann@gmail.com>
2018-03-18 21:29:21 -0500

    Let's try using Tinkerpop instead ...

--- dev-resources/src/hxgm30/graphdb/dev.clj
+++ dev-resources/src/hxgm30/graphdb/dev.clj
@@ -1,9 +1,11 @@
 (ns hxgm30.graphdb.dev
   (:require
     [clj-odbp.core :as odbp]
+    [clojure.data.xml :as xml]
     [clojure.java.io :as io]
     [clojure.pprint :refer [pprint]]
     [clojure.tools.namespace.repl :as repl]
+    [clojurewerkz.ogre.core :as ogre]
     [clojusc.twig :as logger]
     [hxgm30.graphdb.config :as config]
     [hxgm30.graphdb.server :as server]))
@@ -11,4 +13,3 @@
 (logger/set-level! ['hxgm30] :info)
 
 (def refresh #'repl/refresh)
-
