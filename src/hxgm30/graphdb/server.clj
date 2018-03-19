1f1201463368d47d6753c89b707691d066fa1601
Duncan McGreggor <oubiwann@gmail.com>
2018-03-18 21:32:23 -0500

    Updates for new config.

--- src/hxgm30/graphdb/server.clj
+++ src/hxgm30/graphdb/server.clj
@@ -8,19 +8,29 @@
     (com.orientechnologies.orient.server OServerMain))
   (:gen-class))
 
+(defn set-pw!
+  [cfg-edn]
+  (log/debug "Setting ORIENTDB_ROOT_PASSWORD ...")
+  (->> cfg-edn
+       config/user-password
+       (System/setProperty "ORIENTDB_ROOT_PASSWORD")))
+
 (defn set-home!
-  [cfg]
+  [cfg-edn]
   (log/debug "Setting ORIENTDB_HOME ...")
-  (let [home-path (get-in cfg [:orientdb :home])
+  (let [home-path (get-in cfg-edn [:orientdb :home])
         home (.getAbsolutePath (io/file home-path))]
     (System/setProperty "ORIENTDB_HOME" home)))
 
 (defn start
-  [cfg]
+  [cfg-edn]
   (log/debug "Starting OrientDB embedded database ...")
-  (set-home! cfg)
+  (set-home! cfg-edn)
+  (set-pw! cfg-edn)
   (let [server (OServerMain/create)
-        cfg (config/generate-xml-stream cfg)]
+        cfg (config/generate-xml-stream cfg-edn)]
+    (log/debug "server:" server)
+    (log/debug "cfg:" cfg)
     (.startup server cfg)
     (.activate server)
     server))
@@ -32,7 +42,7 @@
 
 (defn -main
   [& args]
-  (let [cfg (config/data)]
-    (logger/set-level! (get-in cfg [:logging :nss])
-                       (get-in cfg [:logging :level]))
-    (start cfg)))
+  (let [cfg-edn (config/data)]
+    (logger/set-level! (get-in cfg-edn [:logging :nss])
+                       (get-in cfg-edn [:logging :level]))
+    (start cfg-edn)))
