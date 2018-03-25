2631813840d185a7dc1a77bb65eeb47dd2c24070
Duncan McGreggor <oubiwann@gmail.com>
2018-03-24 20:18:19 -0500

    Added dev-system support.

--- dev-resources/src/hxgm30/graphdb/dev.clj
+++ dev-resources/src/hxgm30/graphdb/dev.clj
@@ -5,20 +5,85 @@
     [clojure.pprint :refer [pprint]]
     [clojure.tools.namespace.repl :as repl]
     [clojurewerkz.ogre.core :as ogre]
+    [clojusc.dev.system.core :as system-api]
     [clojusc.twig :as logger]
+    [com.stuartsierra.component :as component]
     [hxgm30.graphdb.api.db :as db]
     [hxgm30.graphdb.api.factory :as factory]
-    [hxgm30.graphdb.config :as config]
-    [taoensso.carmine :as redis]
+    [hxgm30.graphdb.components.config :as config]
+    [hxgm30.graphdb.components.core]
+    [hxgm30.graphdb.components.redis :as redis]
+    [taoensso.carmine :as car]
     [trifl.java :refer [show-methods]])
   (:import
     (com.lambdazen.bitsy BitsyGraph)
     (java.net URI)
     (java.nio.file Paths)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Initial Setup & Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (logger/set-level! '[hxgm30] :info)
 
+(def ^:dynamic *mgr* nil)
+
+(defn banner
+  []
+  (println (slurp (io/resource "text/banner.txt")))
+  :ok)
+
+(defn mgr-arg
+  []
+  (if *mgr*
+    *mgr*
+    (throw (new Exception
+                (str "A state manager is not defined; "
+                     "have you run (startup)?")))))
+
+(defn system-arg
+  []
+  (if-let [state (:state *mgr*)]
+    (system-api/get-system state)
+    (throw (new Exception
+                (str "System data structure is not defined; "
+                     "have you run (startup)?")))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   State Management   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn startup
+  []
+  (alter-var-root #'*mgr* (constantly (system-api/create-state-manager)))
+  (system-api/set-system-ns (:state *mgr*) "hxgm30.graphdb.components.core")
+  (system-api/startup *mgr*))
+
+(defn shutdown
+  []
+  (when *mgr*
+    (let [result (system-api/shutdown (mgr-arg))]
+      (alter-var-root #'*mgr* (constantly nil))
+      result)))
+
+(defn system
+  []
+  (system-api/get-system (:state (mgr-arg))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Reloading Management   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn reset
+  []
+  (shutdown)
+  (repl/refresh :after 'hxgm30.graphdb.dev/startup))
+
 (def refresh #'repl/refresh)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Test Specs   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def orientdb-spec
   {:protocol "remote"
@@ -36,12 +101,24 @@
   {:host "localhost"})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;   Redis   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Data   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn conn
+  []
+  (redis/get-conn (system)))
+
+(defn cypher
+  [query-str]
+  (db/cypher (conn) query-str))
+
+(defn dump
+  []
+  (db/dump (conn)))
 
 (comment
   (def f (factory/create :redis redis-spec))
-  (def g (factory/connect :game f))
+  (def g (factory/connect f :game))
   ;; If you haven't created any vertices:
   (def cave (db/add-vertex g {:type :room :name "A cave" :description "You are in a dark cave."}))
   (def tunnel (db/add-vertex g {:type :room
