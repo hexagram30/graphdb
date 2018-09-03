58411a2916910a334900c70082d8858e30b95e55
Duncan McGreggor <oubiwann@gmail.com>
2018-09-03 11:00:01 -0500

    Removed old plugins.

--- plugins/redis/src/hxgm30/graphdb/plugin/redis/api/util.clj
+++
@@ -1,7 +0,0 @@
-(ns hxgm30.graphdb.plugin.redis.api.util
-  (:require
-    [clojure.string :as string]))
-
-(defn get-uuid
-  [id]
-  (last (string/split id #":")))
--- plugins/redisgraph/hxgm30/graphdb/plugin/redis/api/util.clj
+++
@@ -1,5 +0,0 @@
-(ns hxgm30.graphdb.plugin.redisgraph.api.util)
-
-(defn parse-node-props
-  [props]
-  [(:label props) (dissoc props :label)])
